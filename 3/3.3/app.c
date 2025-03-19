23311e0e619ed57e004f9fc8486cc3c8f1c324e9
hello <seelater1337@gmail.com>
2025-03-19 21:32:10 +0300

    2/3

--- 3/3.3/app.c
+++ 3/3.3/app.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <time.h>
+
 
 #define SIZE 1000
-
-
 
 void expand(char s1[], char s2[])
 {
@@ -42,6 +42,10 @@ void expand(char s1[], char s2[])
                 mid=end;
                 end = s1[i];
                 printf("mid %c, end %c\n",mid,end);
+            }else if (state !=0 && end == '0') 
+            {
+                start ='0';
+                state =0;
             }
 
         }
@@ -62,6 +66,7 @@ void expand(char s1[], char s2[])
         {
             state++;
         }
+
         if(n_start!='0' && s1[i]=='-')
         {
             n_state++;
@@ -73,6 +78,8 @@ void expand(char s1[], char s2[])
     
     // loop to fill s2
     //
+
+
     if (start !='0'  && end !='0' )
     {
         if (mid != '0')
@@ -106,7 +113,6 @@ void expand(char s1[], char s2[])
             n_start++;
             j++;
         }
-
     }
     
     s2[j]='\0';
@@ -115,13 +121,65 @@ void expand(char s1[], char s2[])
 
 }
 
+void expand2(char s1[],char s2[])
+{
+
+    int i,j;
+    int state =0;
+    int in_abc =0;
+    char c;
+
+    for (i=0,j=0;s1[i]!='\0';i++)
+    {
+        if((s1[i]>='a' &&  s1[i]<='z') || (s1[i]>='A' && s1[i]<='Z')  
+                                       || (s1[i]>='1'&& s1[i]<='9')) 
+        {
+            in_abc = 1;
+            if (state ==1)
+            {
+                while(c<= s1[i])
+                {
+                    s2[j++] = c++;
+                }
+            }
+        }else if(s1[i]==' ') {
+            s2[j++]=s1[i];
+            in_abc=0;
+        }else if(state==0)
+        {
+            s2[j++]=s1[i];
+        }
+
+        if(in_abc == 1 && s1[i]=='-')
+        {
+            state = 1 ;
+            c = s1[i-1];
+        }                   
+    }
+
+    s2[j]='\0';
+    printf("%s\n",s2);
+}
+
+
 
 int main(void)
 {
-    char s1[] ="-a-c";
+    char s1[] ="a-z1-4";
     char s2[SIZE];
-    
+    clock_t begin = clock();
     expand(s1,s2);
-    
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%lf\n",time_spent);
+
+    begin = clock();
+
+    expand2(s1,s2);
+    end = clock();
+     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%lf\n",time_spent);
+
+
     return 0;
 }
