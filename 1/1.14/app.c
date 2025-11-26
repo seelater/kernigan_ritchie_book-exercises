e94dce68b54ca0e27228fb4dedd134eb23de441b
hello <seelater1337@gmail.com>
2025-11-26 09:36:13 +0300

    1.14

--- 1/1.14/app.c
+++ 1/1.14/app.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 #define ENG_SIZE 26
 #define NUM_SIZE 10
 
 int main()
 {
-    int c = 0;
-    int symbols[ENG_SIZE] = {0};
-    int numbers[NUM_SIZE] = {0}; 
-    while ( (c = getchar())!=EOF)
-    {
-        if (c >='a'&& c <='z')
-        {
+    int c;
+    int symbols[ENG_SIZE];
+    int numbers[NUM_SIZE]; 
+
+    /*init 0*/
+    for (size_t i = 0; i < ENG_SIZE; ++i) {
+        symbols[i] = 0;
+    }
+    for (size_t i = 0; i < NUM_SIZE; ++i) {
+        numbers[i] = 0;
+    }
+    while ((c = getchar()) != EOF) {
+        if (c >='a' && c <='z') {
             ++symbols[c - 'a'];
-        }else if ( c >= '0' && c <= '9')
-        {   
+        }else if (c >= '0' && c <= '9') {   
             ++numbers[c - '0'];
         }
     }
-    for (int i = 0; i<ENG_SIZE; i++)
-    {
-        printf("%2i ",i);
-        for (int j = 1; j <= symbols[i]; j++)
-        {
-            printf("=");
-        }
-        printf("\n");
-    }
-    puts("-----------------");
-    for ( int i = 0; i<NUM_SIZE; i++)
-    {
-        printf("%2i ",i);
-        for (int j = 1; j <= numbers[i]; j++)
-        {
+    /*output*/
+    for (int i = 0; i < ENG_SIZE; i++) {
+        printf("%c ", i + 'a');
+        for (int j = 1; j <= symbols[i]; j++) {
             printf("=");
         }
         printf("\n");
     }
 
-    return 0;
+    puts("-----------------");
+    for ( int i = 0; i<NUM_SIZE; i++) {
+        printf("%i ",i);
+        for (int j = 1; j <= numbers[i]; j++) {
+            printf("=");
+        }
+        printf("\n");
+    }
+
+    return EXIT_SUCCESS;
 }
